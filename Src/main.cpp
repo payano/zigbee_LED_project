e8b78650bb45df0bd5c17c990b4f427b22d0a8cf
Johan Svensson <johan@exclude.se>
2017-12-20 20:27:13 +0100

    Lowered pwm to 10khz.

--- Src/main.cpp
+++ Src/main.cpp
@@ -343,7 +343,7 @@ static void MX_TIM3_Init(void)
   TIM_OC_InitTypeDef sConfigOC;
 
   htim3.Instance = TIM3;
-  htim3.Init.Prescaler = 6;
+  htim3.Init.Prescaler = 19;
   htim3.Init.CounterMode = TIM_COUNTERMODE_UP;
   htim3.Init.Period = 254;
   htim3.Init.ClockDivision = TIM_CLOCKDIVISION_DIV4;
